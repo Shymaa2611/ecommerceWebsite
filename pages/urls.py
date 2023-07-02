49f51336191d8be6981ac36421237d9fde1d01c7
Your Name <you@example.com>
2023-07-02 18:17:27 +0200

    Order and cart Pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('profile/',views.profile,name='profile'),
+    path('edit_profile/',views.edit_profile,name='edit_profile'),
+    path('single_product/',views.single_product,name='single_product'),
+    path('signUp/',views.signUp,name='signUp'),
+    path('login/',views.login,name='login'),
+    path('card/',views.card,name='card'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('order_success/',views.order_success,name='order_success'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'pages/index.html')
+def profile(request):
+    return render(request,'pages/profile.html')
+def edit_profile(request):
+    return render(request,'pages/edit_profile.html')
+def single_product(request):
+    return render(request,'pages/single_product.html')
+def signUp(request):
+    return render(request,'authentication/signUp.html')
+def card(request):
+    return render(request,'pages/card.html')
+def checkout(request):
+    return render(request,'pages/checkout.html')
+def order_success(request):
+    return render(request,'pages/order_success.html')
+def login(request):
+    return render(request,'authentication/login.html')
